279c74204f841da637b239a40d17e84549e3cab6
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2019-04-28 20:51:08 +0100

    Added second layer in API. Adapted styles

--- src/app/home-details/home-details.page.ts
+++ src/app/home-details/home-details.page.ts
@@ -19,10 +19,21 @@ export class HomeDetailsPage implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private homeService: HomeService) { }
 
   ngOnInit() {
-    // pulling our id from the url parameters for the character we need to make a request for
+  //   // pulling our id from the url parameters for the character we need to make a request for
     let id = this.activatedRoute.snapshot.paramMap.get('id');
-    //callin api with id, replacing info null with response
+  //   //callin api with id, replacing info null with response
     this.homeService.getDetails(id)
-      .subscribe(res => this.information = res);
+      .subscribe(res => {
+        const details = {
+          ...res
+        }
+        this.homeService.getAll(res.films).subscribe(res => details.films = res);
+        // this.homeService.getAll(res.species).subscribe(res => details.species = res);
+        this.homeService.getAll(res.vehicles).subscribe(res => details.vehicles = res);
+        // this.homeService.getAll(res.starships).subscribe(res => details.starships = res);
+        // this.homeService.getAll([res.homeworld]).subscribe(res => details.homeworld = res);
+        // console.log(details)
+        this.information = details
+      });
   }
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 })
 
 export class HomeService {
-  url = 'https://swapi.co/api/people';
+  url = 'https://swapi.co/api';
 
   /* aliasing the HttpClient module to the private variable 'http'
    in the constructor of this class */
@@ -16,10 +16,15 @@ export class HomeService {
 
   /* making request to star wars api for all people data */
   getData(): Observable<any> {
-    return this.http.get(this.url);
+    return this.http.get(`${this.url}/people`);
   }
 
   getDetails(id) {
-    return this.http.get(`${this.url}/${id}`);
+    return this.http.get(`${this.url}/people/${id}`);
+  }
+
+  // using fork Join similar to promise all to get arrays of data
+  getAll(urls) {
+    return forkJoin(urls.map(url => this.http.get(url)));
   }
 }
